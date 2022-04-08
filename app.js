4ab51f62064dab67d66548dfb2b162740662bc9e
ro-el-c <chung3286@naver.com>
2022-04-08 23:32:00 +0900

    add Booleans

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-console.log("print")
-console.log(5*2)
+// console.log("print")
+// console.log(5*2)
 
-const a = 5;
-const b = 3;
-let myName = "ro";
+// const a = 5;
+// const b = 3;
+// let myName = "ro";
 
-console.log(a);
-console.log(b);
-console.log("My name : " + myName);
+// console.log(a);
+// console.log(b);
+// console.log("My name : " + myName);
 
-myName = "ro-el";
+// myName = "ro-el";
 
-console.log("Changed my name : " + myName);
+// console.log("Changed my name : " + myName);
+
+
+const amIFat = false;
+const somethingThere = null;
+let something;
+
+console.log(amIFat);
+console.log(somethingThere);
+console.log(something);
