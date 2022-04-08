a76725cd45aa0504bce4db2b3d05729b071e6c17
ro-el-c <chung3286@naver.com>
2022-04-08 23:24:49 +0900

    add const and let

--- app.js
+++ app.js
@@ -2,5 +2,13 @@ console.log("print")
 console.log(5*2)
 
 const a = 5;
+const b = 3;
+let myName = "ro";
 
-console.log(a/2)
+console.log(a);
+console.log(b);
+console.log("My name : " + myName);
+
+myName = "ro-el";
+
+console.log("Changed my name : " + myName);
