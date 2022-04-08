ecba60494386aaae6e9261fe6f1d7102cb5ec563
ro-el-c <chung3286@naver.com>
2022-04-08 23:54:26 +0900

    add Arrays

--- app.js
+++ app.js
@@ -14,10 +14,35 @@
 // console.log("Changed my name : " + myName);
 
 
-const amIFat = false;
-const somethingThere = null;
-let something;
+// const amIFat = false;
+// const somethingThere = null;
+// let something;
 
-console.log(amIFat);
-console.log(somethingThere);
-console.log(something);
+// console.log(amIFat);
+// console.log(somethingThere);
+// console.log(something);
+
+
+const mon = "mon";
+const tue = "tue";
+const wed = "wed";
+const thu = "thu";
+const fri = "fri";
+const sat = "sat";
+const sun = "sun";
+
+const dow = mon + tue + wed + thu + fri + sat + sun;
+console.log(dow);
+
+
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+console.log(daysOfWeek);
+
+// Get Item from Array
+console.log(daysOfWeek[2]);
+
+// Add one more day to the array
+daysOfWeek.push("sun");
+console.log(daysOfWeek);
+
+
